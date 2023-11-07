79323238f8d36e560adc506b7366c7058ac549dd
Wenqian Wang <hardwaregore101@gmail.com>
2023-11-07 13:49:47 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,44 @@
 
 # Install packages:
 
+echo "Installing Packages"
+
 apt update
-apt install openssh-server openssh-client neovim sudo net-tools -y
+apt install openssh-server openssh-client neovim sudo net-tools wget curl -y
 apt upgrade -y
 
-# Install XAMPP:
+# Download XAMPP:
 
+echo "Enter XAMPP installer script IP:"
+read ip
+
+wget $ip/xampp.run
+
+chmod +x ./xampp.run
+
+# Create Directories:
+
+mkdir /show
+mkdir /git
+
+# Grant Permissions
+
+chmod 777 /show
+chmod 777 /git
+
+# Post install
+
+echo "After XAMPP has finished installing, run the following commands:"
+echo "ssh-keygen"
+echo "cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys"
+
+echo ""
+
+echo "Then, add the following into /etc/sudoers"
+echo "daemon    ALL=(ALL) NOPASSWD: ALL"
+
+echo ""
+
+# Install XAMPP
+
+./xampp.run
